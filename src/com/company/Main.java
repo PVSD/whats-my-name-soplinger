a8da5285d8ff338b7b03144f568d653274394d7d
soplinger <solekibbles4@gmail.com>
2018-09-10 21:09:48 -0400

    when the blackout go blackout the shelter go open

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
-
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner tapTapRead = new Scanner(System.in);
+
+        Scanner bigTapTapRead = new Scanner(System.in);
+
+        System.out.println("What is thous't name?");
+
+        String f = tapTapRead.next();
+
+        System.out.println("What is your last name?");
+
+        String l = bigTapTapRead.next();
+
+        System.out.println("Your full name is " + f + " " + l);
+
+
+
+
+
     }
 }
+d
